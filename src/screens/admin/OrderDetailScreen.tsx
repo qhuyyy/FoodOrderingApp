098edc50dd61e69a0677d6f885e4235da85b6b43
qhuyyy <bqhuy2211@gmail.com>
2025-06-18 08:55:02 +0700

    Admin - Order's status selector

--- src/screens/admin/OrderDetailScreen.tsx
+++ src/screens/admin/OrderDetailScreen.tsx
@@ -1,11 +1,11 @@
-import { FlatList, StyleSheet, Text, View } from 'react-native';
-import React, { useLayoutEffect } from 'react';
+import { FlatList, Pressable, StyleSheet, Text, View } from 'react-native';
+import React, { useLayoutEffect, useState } from 'react';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { UserOrderStackParamList } from '../../navigation/UserOrderNavigator';
 import { RouteProp, useNavigation, useRoute } from '@react-navigation/native';
 import OrderListItem from '../../components/OrderListItem';
 import OrderItemListItem from '../../components/OrderItemListItem';
-
+import { OrderStatus } from '../../type/types';
 export type NavigationProp = NativeStackNavigationProp<
   UserOrderStackParamList,
   'OrderDetail'
@@ -17,6 +17,10 @@ const OrderDetailScreen = () => {
   const route = useRoute<ProductDetailRouteProp>();
 
   const { order } = route.params;
+
+  const [currentStatus, setCurrentStatus] = useState(order.status);
+
+  const statuses: OrderStatus[] = ['New', 'Cooking', 'Delivering', 'Delivered'];
 
   useLayoutEffect(() => {
     navigation.setOptions({ title: `Order #${order.id}` });
@@ -30,6 +34,28 @@ const OrderDetailScreen = () => {
         renderItem={({ item }) => <OrderItemListItem item={item} />}
         contentContainerStyle={{ gap: 2 }}
       />
+      <Text>Status</Text>
+      <View style={styles.statuses}>
+        {statuses.map(size => (
+          <Pressable
+            key={size}
+            style={[
+              styles.status,
+              currentStatus === size && styles.currentStatus,
+            ]}
+            onPress={() => setCurrentStatus(size)}
+          >
+            <Text
+              style={[
+                styles.statusText,
+                currentStatus === size && styles.currentStatusText,
+              ]}
+            >
+              {size}
+            </Text>
+          </Pressable>
+        ))}
+      </View>
     </View>
   );
 };
@@ -40,5 +66,24 @@ const styles = StyleSheet.create({
   container: {
     gap: 10,
     padding: 10,
+  },
+  statuses: {
+    flexDirection: 'row',
+    gap: 5,
+  },
+  status: {
+    borderWidth: 1,
+    padding: 8,
+    borderRadius: 5,
+  },
+  statusText: {
+    fontWeight: '500',
+    fontSize: 14,
+  },
+  currentStatus: {
+    backgroundColor: '#96C9DC',
+  },
+  currentStatusText: {
+    color: 'white',
   },
 });
